e976f68cec24e27c89a58c173871b3f8808947ea
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-03-22 20:53:42 +0300

    DBStorage - Place

--- models/city.py
+++ models/city.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from os import getenv
 from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade="all, delete", backref="cities")
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(
+                String(60), ForeignKey('states.id'), nullable=False)
+
+        places = relationship(
+                "Place", back_populates="cities", cascade="all, delete")
+    else:
+        name = ''
+        state_id = ''
+
+    # One-to-many relationship with Place, with automatic deletion
+    # of linked Place objects
+    places = relationship(
+            "Place", back_populates="cities", cascade="all, delete")
