41fa9a4fbe109cb771805d8a4ecd5bca4f8dd2cb
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-03-22 12:18:24 +0300

    DBStorage - User

--- models/user.py
+++ models/user.py
@@ -9,13 +9,7 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
 
-    if getenv('HBN_TYPE_STORAGE') == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
