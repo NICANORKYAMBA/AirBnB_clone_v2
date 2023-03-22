3beeb5a8b013d2dfb35377199ac1122272089349
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-03-23 00:22:16 +0300

    DBStorage - Amenity

--- models/amenity.py
+++ models/amenity.py
@@ -2,16 +2,16 @@
 """Amenity Module for HBNB project"""
 from os import getenv
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Table
+from sqlalchemy import Column, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
 
-association_table = Table('place_amenity', Base.metadata,
+place_amenity= Table('place_amenity', Base.metadata,
                           Column(
-                              'place_id', String(60), primary_key=True,
+                              'place_id', String(60), ForeignKey('places.id'),
                               nullable=False),
                           Column(
-                              'amenity_id', String(60), primary_key=True,
+                              'amenity_id', String(60), ForeignKey('amenities.id'),
                               nullable=False))
 
 
@@ -24,7 +24,7 @@ class Amenity(BaseModel, Base):
         name = Column(String(128), nullable=False)
 
         place_amenities = relationship(
-                "Place", secondary=association_table,
+                "Place", secondary=place_amenity,
                 backref="amenities")
     else:
         name = ''
--- models/place.py
+++ models/place.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+import models
 from os import getenv
+from models.review import Review
+from models.amenity import Amenity, place_amenity
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship
 
 
@@ -11,15 +14,6 @@ class Place(BaseModel, Base):
     __tablename__ = 'places'
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        metadata = Base.metadata
-        place_amenity = Table('place_amenity', metadata,
-                              Column('place_id', String(60),
-                                     ForeignKey('places.id'),
-                                     primary_key=True, nullable=False),
-                              Column('amenity_id', String(60),
-                                     ForeignKey('amenities.id'),
-                                     primary_key=True, nullable=False))
-
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
@@ -31,11 +25,24 @@ class Place(BaseModel, Base):
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
 
+        reviews = relationship("Review", backref="place", cascade="delete")
         amenities = relationship(
-                "Amenity", secondary=place_amenity, viewonly=False)
-        user = relationship("User", back_populates="places")
-        cities = relationship("City", back_populates="places")
+                "Amenity", secondary=place_amenity,
+                back_populates='place_amenities', viewonly=False)
+        amenity_ids = []
     else:
+        city_id = ''
+        user_id = ''
+        name = ''
+        description = ''
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
         @property
         def reviews(self):
             """
