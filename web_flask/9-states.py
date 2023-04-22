fe3012208da38838a1ff89d879567996e309c2d9
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-04-22 15:50:49 +0300

    States and State

---
+++ web_flask/9-states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module for a Flask web application to manage states and cities.
+"""
+
+from flask import Flask, render_template
+from models import storage, State, City
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Closes the database at the end of the request."""
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def states_list():
+    """Displays a list of all the states."""
+    states = storage.all(State).values()
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_cities_list(id):
+    """Displays a list of all the cities in a state."""
+    state = storage.get(State, id)
+    if state is None:
+        return render_template('9-states.html')
+    cities = state.cities
+    return render_template('9-states.html', state=state, cities=cities)
+
+
+if __name__ == '__main__':
+    """Start Flask web application"""
+    app.run(host='0.0.0.0', port=5000)
