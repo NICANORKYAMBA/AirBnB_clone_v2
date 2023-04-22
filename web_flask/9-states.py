4fa165349adf620e93d4f9a6b326603f97ebc1b5
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-04-22 16:45:42 +0300

    States and State fixed errors

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -5,32 +5,31 @@ Module for a Flask web application to manage states and cities.
 
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 
 
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the database at the end of the request."""
-    storage.close()
-
-
 @app.route('/states', strict_slashes=False)
 def states_list():
     """Displays a list of all the states."""
-    states = storage.all(State)
+    states = storage.all("State")
     return render_template('9-states.html', state=states)
 
 
 @app.route('/states/<id>', strict_slashes=False)
 def state_cities_list(id):
     """Displays a list of all the cities in a state."""
-    for state in storage.all(State).values():
+    for state in storage.all("State").values():
         if state.id == id:
             return render_template('9-states.html', state=state)
     return render_template('9-states.html')
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Closes the database at the end of the request."""
+    storage.close()
 
 
 if __name__ == '__main__':
