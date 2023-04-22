14bc68d06bfb5a6185a3e751108956a7f44ddae8
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-04-22 16:55:05 +0300

    States and State fixed errors

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,37 +1,42 @@
 #!/usr/bin/python3
-"""
-Module for a Flask web application to manage states and cities.
-"""
+"""Starts a Flask web application.
 
-from flask import Flask, render_template
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states: HTML page with a list of all State objects.
+    /states/<id>: HTML page displaying the given state with <id>.
+"""
 from models import storage
-
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-def states_list():
-    """Displays a list of all the states."""
+@app.route("/states", strict_slashes=False)
+def states():
+    """Displays an HTML page with a list of all States.
+
+    States are sorted by name.
+    """
     states = storage.all("State")
-    return render_template('9-states.html', states=states)
+    return render_template("9-states.html", state=states)
 
 
-@app.route('/states/<id>', strict_slashes=False)
-def state_cities_list(id):
-    """Displays a list of all the cities in a state."""
-    state = storage.get("State", id)
-    if state:
-        return render_template('9-states.html', state=state)
-    return render_template('9-states.html')
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the database at the end of the request."""
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-if __name__ == '__main__':
-    """Start Flask web application"""
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
